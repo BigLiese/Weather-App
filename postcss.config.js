8911be7c2d33c22a4d8fd686341977344d2e2711
LieseLu <1300455270@qq.com>
2024-05-02 18:41:32 +0800

    convert px to rem

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("postcss-pxtorem")({
+      rootValue: 10,
+      unitPrecision: 5,
+      propList: ["*"],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0,
+      exclude: /node_modules/i,
+    }),
+  ],
+};
