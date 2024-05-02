b5f3b32d24f8210f03bb14cbd2d82c7e149a8ac0
LieseLu <1300455270@qq.com>
2024-05-02 19:33:59 +0800

    extract data to data folder

--- src/pages/App/app.js
+++ src/pages/App/app.js
@@ -1,36 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, useNavigate, useParams } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Select, Flex } from "antd";
 import Home from "../Home/home";
 import NotFoundPage from "../NotFoundPage";
-
+import data from "../../data/cities.json";
 const App = () => {
-  const cities = [
-    {
-      id: 1809858,
-      name: "Guangzhou",
-    },
-    {
-      id: 1816670,
-      name: "Beijing",
-    },
-    {
-      id: 1796236,
-      name: "Shanghai",
-    },
-    {
-      id: 1795565,
-      name: "Shenzhen",
-    },
-    {
-      id: 1796989,
-      name: "Changsha",
-    },
-  ];
-
   const navigate = useNavigate();
-  const currentCity = useParams();
-  console.log(currentCity);
   const [city, setCity] = useState(localStorage.getItem("city") || "guangzhou");
 
   useEffect(() => {
@@ -53,7 +28,7 @@ const App = () => {
               width: 120,
             }}
             onChange={handleCitySelect}
-            options={cities.map((city) => ({
+            options={data.cities.map((city) => ({
               value: city.name,
               label: city.name,
             }))}
@@ -63,7 +38,7 @@ const App = () => {
       <Routes>
         <Route path="/">
           <Route index element={<Home city="guangzhou" />} />
-          {cities.map((city) => (
+          {data.cities.map((city) => (
             <Route
               key={city.name}
               path={city.name.toLowerCase()}
